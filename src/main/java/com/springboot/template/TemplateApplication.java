76db404ac6a7f8ec19a3cb150909a7a48faa6831
PakiWolvorine <farhanhbk@hotmail.com>
2020-05-13 02:51:55 +0300

    intelliJ commit

--- src/main/java/com/springboot/template/TemplateApplication.java
+++ src/main/java/com/springboot/template/TemplateApplication.java
@@ -1,13 +1,22 @@
 package com.springboot.template;
-
+//https://github.com/farhanx/java_spring_boot_app_template
+import com.springboot.template.services.UserRegistrationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TemplateApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserRegistrationService.class);
+
     public static void main(String[] args) {
         SpringApplication.run(TemplateApplication.class, args);
+        logger.info("Template Author's link - https://github.com/farhanx/java_spring_boot_app_template -");
+
+
+
     }
 
 }
